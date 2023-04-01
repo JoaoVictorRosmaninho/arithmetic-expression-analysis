b805cd6fc7b2452a97f88cb3ca8862c4d36d213f
JoaoVictorRosmaninho <joaovictorrosmaninho@gmail.com>
2023-03-31 21:54:24 -0300

    refactor: remove old and useless files

--- libMemory/mem.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <stdlib.h>
-
-double *mem_arrayAlloc(int size, double setValue);
-
-void memclear(void *ptr, int8_t value, size_t size);
-
-void mem_memcpy(void *dest, void *orig, size_t size);
